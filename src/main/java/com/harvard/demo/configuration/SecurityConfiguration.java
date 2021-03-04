8d135bf6d05419373276492947ed470b9283eb3c
salimov-bilol <salimovb1999@gmail.com>
2021-03-04 07:36:10 +0500

    Security Configuration added with in-memory authentication and NoOp password encoder

---
+++ src/main/java/com/harvard/demo/configuration/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.harvard.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().
+                withUser("harvard").
+                password("harvard12345").
+                roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
