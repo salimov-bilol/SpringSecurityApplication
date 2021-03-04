96d6826fdf3e2036efa6b4043d472b85295153c0
salimov-bilol <salimovb1999@gmail.com>
2021-03-04 18:31:49 +0500

    UserDetailsService using Lambda and BCryptPasswordEncoder added

--- src/main/java/com/harvard/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/harvard/demo/configuration/SecurityConfiguration.java
@@ -1,27 +1,37 @@
 package com.harvard.demo.configuration;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.harvard.demo.entity.MyUserDetails;
+import com.harvard.demo.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
+    private final UserRepository userRepository;
 
-    public SecurityConfiguration(@Qualifier("MyUserDetailsService") UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public SecurityConfiguration(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return username -> this.userRepository
+                .findByUsername(username)
+                .map(MyUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("The user does not exist in the database."));
     }
 
     @Override
@@ -36,6 +46,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(12);
     }
 }
